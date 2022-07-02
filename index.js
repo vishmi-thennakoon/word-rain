2d16a98f911e68f08384954b4eafb226585ab94f
thennakoonvishmi <thennakoonvishmi@gmail.com>
2022-07-02 14:21:26 +0530

    Show drop image for no text

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const colors = ['red', 'green', 'yellow', 'brown', 'blue', 'purple', 'orange']
-document.querySelector("input").addEventListener("keydown", e =>{
-    if(e.key === 'Enter'){
+document.querySelector("input").addEventListener("keydown", event =>{
+    if(event.key === 'Enter'){
         for(let i=0; i<120; i++) {
             const drop = document.createElement("div")
-            drop.innerHTML = e.target.value
+            drop.innerHTML = event.target.value
             drop.className = "drop"
             drop.style.left = window.innerWidth*(Math.random())
             +'px'
             drop.style.animationDelay = i/100+"s"
             const index = Math.floor(Math.random()*colors.length)
             drop.style.color = colors[index]
+            if(event.target.value === ''){
+                drop.classList.add("noimage")
+            }
             document.querySelector(".rain").appendChild(drop)
         
-            
         }
-        e.target.remove()
+        event.target.remove()
     }
 
 
